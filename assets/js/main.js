1e639eda755aabbaec28991ad5547189ce084ddf
Benjaimico <benja124@live.cl>
2023-01-16 12:04:34 -0300

    js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,28 +1,28 @@
 function muestraReloj() {
-   
+
     var fechaHora = new Date();
     var horas = fechaHora.getHours();
     var minutos = fechaHora.getMinutes();
     var segundos = fechaHora.getSeconds();
     var day = fechaHora.getDay();
     if (day <= 9) {
-      day = '0' + day
-    } 
-    var month = (fechaHora.getMonth()+1); 
+        day = '0' + day
+    }
+    var month = (fechaHora.getMonth() + 1);
     if (month <= 9) {
-      month = '0' + month
-    } 
+        month = '0' + month
+    }
     var year = fechaHora.getFullYear();
-    var fecha = day +'/'+ month +'/'+ year;
+    var fecha = day + '/' + month + '/' + year;
 
-    if(horas < 10) { horas = '0' + horas; }
-    if(minutos < 10) { minutos = '0' + minutos; }
-    if(segundos < 10) { segundos = '0' + segundos; }
+    if (horas < 10) { horas = '0' + horas; }
+    if (minutos < 10) { minutos = '0' + minutos; }
+    if (segundos < 10) { segundos = '0' + segundos; }
 
 
-    document.getElementById("reloj").innerHTML = fecha +'  '+ horas+':'+minutos+':'+segundos;
-  }
+    document.getElementById("reloj").innerHTML = fecha + '  ' + horas + ':' + minutos + ':' + segundos;
+}
 
-    window.onload = function() {
+window.onload = function () {
     setInterval(muestraReloj, 1000);
-  }
+}
